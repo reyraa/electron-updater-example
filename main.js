9cfee0419b0353f2149f014aad91904733e55a45
reyraa <ali@lightcurve.io>
2021-09-03 11:30:33 +0200

    Bump v12.3.0

--- main.js
+++ main.js
@@ -59,7 +59,12 @@ function sendStatusToWindow(text) {
   win.webContents.send('message', text);
 }
 function createDefaultWindow() {
-  win = new BrowserWindow();
+  win = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+  });
   win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
